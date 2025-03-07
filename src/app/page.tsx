1500b5726b96dda99b7dde988bca648d6395a497
ADJAHOTO <benisseadjahoto@gmail.com>
2025-03-07 12:36:22 +0100

    Premier Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold bg-orange-500 w-full x-48 p-2 mt-2 " >
+        Hello World
+      </h1>
+      <p>This is a page.</p>
+    </div>
+  );
+};
